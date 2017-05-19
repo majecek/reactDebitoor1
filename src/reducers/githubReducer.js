471a00ba26d8344e2b9ef518792a2a2131a2ccb1
Marek Chowaniok <marek.chowaniok@gmail.com>
2017-05-19 11:41:04 +0200

    fix: refactoring of githubreducer + tests passing

--- src/reducers/githubReducer.js
+++ src/reducers/githubReducer.js
@@ -1,58 +1,78 @@
 import axios from 'axios'
+import C from '../constants'
 
 // ACTION TYPES
-const REPOS_ORDERED_BY_STARS = 'REPOS_ORDERED_BY_STARS'
+const LOAD_REPOS_ORDERED_BY_STARS = 'REPOS_ORDERED_BY_STARS'
+const LOAD_REPOS_ORDERED_BY_STARS_FAIL = 'LOAD_REPOS_ORDERED_BY_STARS_FAIL'
+const SET_SEARCH_TERM = 'SET_SEARCH_TERM'
 const PULL_REQUESTS = 'PULL_REQUESTS'
-const URL_REPOS_ORDERED_BY_STARS_PREFIX = 'https://api.github.com/search/repositories?q='
-const URL_REPOS_ORDERED_BY_STARS_POSTFIX = 'stars:%3E1&sort=stars&order=desc'
-const URL_PULL_REQUESTS = 'https://api.github.com/repos/'
-const initialState = {}
-let searchTerm = ''
+const PULL_REQUESTS_FAIL = 'PULL_REQUESTS_FAIL'
+
+const initialState = {
+  searchTerm: '',
+  repos: []
+}
 
 // REDUCERS
 export default function (state = initialState, action = {}) {
   switch (action.type) {
-
-    case REPOS_ORDERED_BY_STARS:
+    case LOAD_REPOS_ORDERED_BY_STARS:
       return {
         ...state,
-        repos: action.payload.data,
+        repos: action.payload
       }
-
+    case LOAD_REPOS_ORDERED_BY_STARS_FAIL:
+      return {
+        ...state,
+        error: action.error
+      }
     case PULL_REQUESTS:
       return {
         ...state,
-        pullRequests: action.payload.data
+        pullRequests: action.payload
       }
-
     default:
       return state
   }
 }
 
 // ACTIONS
-export function getRepositoryOrderedByStars (term = searchTerm) {
-  let URL = ''
-  if (term) {
-    URL = `${URL_REPOS_ORDERED_BY_STARS_PREFIX}${term}?${URL_REPOS_ORDERED_BY_STARS_POSTFIX}`
-  } else {
-    URL = `${URL_REPOS_ORDERED_BY_STARS_PREFIX}${URL_REPOS_ORDERED_BY_STARS_POSTFIX}`
-  }
-  const request = axios.get(`${URL}`)
-  searchTerm = term
-
-  return {
-    type: REPOS_ORDERED_BY_STARS,
-    payload: request
+export function getRepositoryOrderedByStars (term) {
+  const url = term
+  ? `${C.URL_REPOS_ORDERED_BY_STARS_PREFIX}${term}?${C.URL_REPOS_ORDERED_BY_STARS_POSTFIX}`
+  : `${C.URL_REPOS_ORDERED_BY_STARS_PREFIX}${C.URL_REPOS_ORDERED_BY_STARS_POSTFIX}`
+  return dispatch => {
+    axios.get(url)
+      .then(response => {
+        dispatch({
+          type: LOAD_REPOS_ORDERED_BY_STARS,
+          payload: response.data.items
+        })
+      })
+      .catch(error => {
+        dispatch({
+          type: LOAD_REPOS_ORDERED_BY_STARS_FAIL,
+          error
+        })
+      })
   }
 }
 
 export function getPullRequests (repoUrl) {
-  const URL = `${URL_PULL_REQUESTS}${repoUrl}/pulls`
-  const request = axios.get(`${URL}`)
-
-  return {
-    type: PULL_REQUESTS,
-    payload: request
+  const url = `${C.URL_PULL_REQUESTS}${repoUrl}/pulls`
+  return (dispatch) => {
+    axios.get(`${url}`)
+      .then(response => {
+        dispatch({
+          type: PULL_REQUESTS,
+          payload: response.data
+        })
+      })
+      .catch(error => {
+        dispatch({
+          type: PULL_REQUESTS_FAIL,
+          error
+        })
+      })
   }
 }
