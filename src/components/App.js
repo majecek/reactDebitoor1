eb57839d9a73236ccec4f65152d066e0157fedd6
Marek Chowaniok <marek.chowaniok@gmail.com>
2017-05-19 11:27:21 +0200

    fix: adding injectTap to fix test

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { AppBar } from 'material-ui'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 
 const container_style = {
   fontFamily: "'Roboto', sans-serif"
@@ -8,13 +10,20 @@ const container_style = {
 
 const titleString = 'Github best repositories'
 
+/**
+ * Material UI requirement
+ * Needed for onTouchTap
+ * more info - http://stackoverflow.com/a/34015469/988941
+ */
+injectTapEventPlugin()
+
 class App extends Component {
   render () {
     return (
       <MuiThemeProvider>
         <div style={container_style}>
           <AppBar title={titleString} showMenuIconButton={false}/>
-          {this.props.children}
+          { this.props.children }
         </div>
       </MuiThemeProvider>
     )
