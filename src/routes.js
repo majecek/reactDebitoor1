d1ffd864ac392a1a51b593646ada266cde03b4ea
Marek Chowaniok <marek.chowaniok@gmail.com>
2017-05-19 10:28:36 +0200

    Feat: not found screen

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './components/App'
-import Overview from './containers/Overview'
-import RepositoryDetail from './containers/RepoDetail'
+import Home from './containers/Home/Home'
+import RepositoryDetail from './containers/RepoDetail/RepoDetail'
+import NotFound from './containers/NotFound/NotFound'
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={Overview}/>
+    <IndexRoute component={Home}/>
     <Route path="/repo/:owner/:repoName" component={RepositoryDetail} />
+    <Route path='*' component={NotFound} status={404} />
   </Route>
 )
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, compose, createStore } from 'redux'
 import ReduxPromise from 'redux-promise'
 import { routerMiddleware } from 'react-router-redux'
 import { browserHistory } from 'react-router'
+import thunk from 'redux-thunk'
 import reducers from './reducers'
 
 const router = routerMiddleware(browserHistory)
@@ -9,7 +10,7 @@ const router = routerMiddleware(browserHistory)
 const store = createStore(
   reducers,
   compose(
-    applyMiddleware(ReduxPromise, router),
+    applyMiddleware(thunk, ReduxPromise, router),
     window.devToolsExtension ? window.devToolsExtension() : f => f,
   ),
 )
